895f740d2d1a2ecbcf2cda5a69a0bdc03f4668aa
AnTo3777 <an.moutier123@gmail.com>
2022-08-23 21:12:52 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,8 +2,8 @@ function changePage() {
     window.location.href="blankpage.html"; 
 };
 
-
-function changePage() {
+function payOneEuro() {
     window.location.href="http://bn.plus/1IW7AA"; 
 };
+
 
