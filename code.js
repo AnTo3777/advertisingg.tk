8230a4b7f0a982d91621885602c1d7b338be46a0
AnTo3777 <an.moutier123@gmail.com>
2022-08-23 21:37:07 +0200

    dez

--- code.js
+++ code.js
@@ -2,8 +2,10 @@ function changePage() {
     window.location.href="blankpage.html"; 
 };
 
-function payOneEuro() {
+function payOneEuroTest() {
     window.location.href="http://bn.plus/1IW7AA"; 
 };
 
-
+function payOneEuro() {
+    window.location.href="http://bn.plus/eEOlsw"; 
+};
